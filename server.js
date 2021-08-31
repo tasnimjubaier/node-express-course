5c9bee75167d231e7d1184f2c9088e06fe9c6868
Tasnim Jubaier <tasnimjubaier@gmail.com>
2021-08-31 15:45:48 +0600

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 
 const mockUserData = [
     {name:'Mark'},
@@ -21,6 +25,29 @@ app.get('/users/:id', function(req, res){
         messege: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/users/login', function(req, res){
+    const username = req.params.name;
+    const password = req.params.password;
+
+    const mockName = "billy";
+    const mockPasswor = "pass";
+
+    if(username === mockName && password == mockPassword){
+        res.json({
+            success: true,
+            message: 'match',
+            token: 'encrypted token goes here'
+        });
+    }
+    else{
+        res.json({
+            success: false,
+            message: 'did not match'
+        });
+    }
+
 })
 
 app.listen(8000, function(){
